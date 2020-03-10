953c79d74833e44be1c0887d6facf4d5d39a30c1
User0017 <user0017@Administrators-MacBook-Pro-3.local>
2020-03-10 20:13:31 +0100

    setting up passwords

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get ('/customers',(req, res)=>{
+    res.send('Hello from customers')
+})
+
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
 });
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'cyf_ecommerce',
-    password: '',
+    password: 'secrets.dbPassword',
     port: 5432
 });
 
@@ -19,3 +24,4 @@ app.get("/hotels", function(req, res) {
         res.json(result.rows);
     });
 });
+
