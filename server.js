41f540a95cec2ae99f57acdb1ba4b9f776f77a73
User0017 <user0017@Administrators-MacBook-Pro-3.local>
2020-03-10 20:28:40 +0100

    updated the endpoints

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const secrets = require('./secrets');
 
-app.get ('/customers',(req, res)=>{
-    res.send('Hello from customers')
-})
-
-app.listen(3000, function() {
-    console.log("Server is listening on port 3000. Ready to accept requests!");
+app.get ('/products',(req, res)=>{
+    pool.query('SELECT * FROM products', (error, result) => {
+        res.json(result.rows);
+    })    
 });
+
+app.get('/customers', (req, res)=> {
+    pool.query('SELECT * FROM customers', (error, result) => {
+        res.json(result.rows);
+    })    
+});
+
+
 
 const { Pool } = require('pg');
 
@@ -16,13 +22,12 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'cyf_ecommerce',
-    password: 'secrets.dbPassword',
+    password: secrets.dbPassword,
     port: 5432
 });
 
-app.get("/hotels", function(req, res) {
-    pool.query('SELECT * FROM hotels', (error, result) => {
-        res.json(result.rows);
-    });
-});
 
+
+app.listen(3000, function() {
+    console.log("Server is listening on port 3000. Ready to accept requests!");
+});
